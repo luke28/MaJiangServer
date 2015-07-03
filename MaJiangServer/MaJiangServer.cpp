239f52eef28ed505297582dc3eaadd70db315bbc
luke28 <dulun2834@126.com>
2015-07-03 13:07:27 +0800

    删MFC

---
+++ MaJiangServer/MaJiangServer.cpp
@@ -0,0 +1,23 @@
+// MaJiangServer.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "MaJiangServer.h"
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 唯一的应用程序对象
+
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	getchar();
+	return 0;
+}
